6cca82a67b276c0d67d77c9056055f37553a3ee9
AlexSavr <as@sava.site>
2021-05-07 12:52:55 +0300

    Add grass platform to game

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import config from './config';
+import block from './assets/blocks/grass1.png';
 
 let gameConfig = {
     ...config,
@@ -17,12 +18,19 @@ function init() {
 
 function preload ()
 {
-    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('block', block);
 }
 
 function create ()
 {
-
+    this.add.tileSprite(709.5, 129, 1280, 129, 'block');
+    this.add.tileSprite(640, 180, 1280, 129, 'block');
+    this.add.tileSprite(709, 230, 1280, 129, 'block');
+    this.add.tileSprite(640, 282, 1280, 129, 'block');
+    this.add.tileSprite(709.5, 333, 1280, 129, 'block');
+    this.add.tileSprite(640, 384, 1280, 129, 'block');
+    this.add.tileSprite(709, 435, 1280, 129, 'block');
+    // this.add.tilemap(0, 0, 'block', '__BASE').setOrigin(0, 0);
 }
 
 function update ()
