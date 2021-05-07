dc2f3f70b65044f93ae431d995e6552431c08475
AlexSavr <as@sava.site>
2021-05-07 18:08:32 +0300

    Game scene code to Object-oriented programming

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import game from './src/game';
+import config from './src/config';
 import 'normalize.css';
 import './src/styles/styles.scss';
+import Phaser from 'phaser';
+import Game from './src/game';
 
 document.addEventListener('DOMContentLoaded', () => {
-    game.init();
+    const gameConfig = {
+        ...config,
+        scene: [ Game ]
+    }
+
+    const game = new Phaser.Game(gameConfig);
 })
--- src/game.js
+++ src/game.js
@@ -1,56 +1,39 @@
 import Phaser from 'phaser';
-import config from './config';
 import block from './assets/blocks/grass1.png';
 import player from './assets/Player/player.png';
 
-let gameConfig = {
-    ...config,
-    scene: {
-        preload,
-        create
+class Game extends Phaser.Scene {
+    constructor() {
+        super();
+    }
+
+    preload () {
+        this.load.image('block', block);
+
+        this.load.spritesheet('player', player, { frameWidth: 86, frameHeight: 170 });
+    }
+
+    create ()
+    {
+        this.add.tileSprite(709.5, 129, 1280, 129, 'block');
+        this.add.tileSprite(640, 180, 1280, 129, 'block');
+        this.add.tileSprite(709, 230, 1280, 129, 'block');
+        this.add.tileSprite(640, 282, 1280, 129, 'block');
+        this.add.tileSprite(709.5, 333, 1280, 129, 'block');
+        this.add.tileSprite(640, 384, 1280, 129, 'block');
+        this.add.tileSprite(709, 435, 1280, 129, 'block');
+    
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2, 3 ] }),
+            frameRate: 12,
+            repeat: 2
+        });
+    
+        const cody = this.add.sprite(600, 370);
+        cody.setScale(1);
+        cody.play('walk'); // https://phaser.io/examples/v3/view/animation/create-animation-from-sprite-sheet
     }
 }
 
-new Phaser.Game(gameConfig);
-
-function init() {
-    console.log('Loading...');
-}
-
-function preload ()
-{
-    this.load.image('block', block);
-
-    this.load.spritesheet('player', player, { frameWidth: 86, frameHeight: 170 });
-}
-
-function create ()
-{
-    this.add.tileSprite(709.5, 129, 1280, 129, 'block');
-    this.add.tileSprite(640, 180, 1280, 129, 'block');
-    this.add.tileSprite(709, 230, 1280, 129, 'block');
-    this.add.tileSprite(640, 282, 1280, 129, 'block');
-    this.add.tileSprite(709.5, 333, 1280, 129, 'block');
-    this.add.tileSprite(640, 384, 1280, 129, 'block');
-    this.add.tileSprite(709, 435, 1280, 129, 'block');
-
-    this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2, 3 ] }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    const cody = this.add.sprite(600, 370);
-    cody.setScale(1);
-    cody.play('walk'); // https://phaser.io/examples/v3/view/animation/create-animation-from-sprite-sheet
-}
-
-function update ()
-{
-
-}
-
-export default {
-    init
-}
+export default Game
