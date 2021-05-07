0092ae5bef04ca08aa76e304235a0e7d1751d09a
AlexSavr <as@sava.site>
2021-05-07 18:25:14 +0300

    Player animations

--- src/game.js
+++ src/game.js
@@ -5,12 +5,14 @@ import player from './assets/Player/player.png';
 class Game extends Phaser.Scene {
     constructor() {
         super();
+
+        this.animationFrameRate = 10;
     }
 
     preload () {
         this.load.image('block', block);
 
-        this.load.spritesheet('player', player, { frameWidth: 86, frameHeight: 170 });
+        this.load.spritesheet('player', player, { frameWidth: 85, frameHeight: 170 });
     }
 
     create ()
@@ -24,15 +26,71 @@ class Game extends Phaser.Scene {
         this.add.tileSprite(709, 435, 1280, 129, 'block');
     
         this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2, 3 ] }),
-            frameRate: 12,
-            repeat: 2
+            key: 'walkRight',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2, 3, 4, 5 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkRightTop',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 6, 7, 8, 9, 10, 11 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkTop',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 12, 13, 14, 15, 16, 17 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkLeftTop',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 18, 19, 20, 21, 22, 23 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkLeft',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 24, 25, 26, 27, 28, 29 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkLeft',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 24, 25, 26, 27, 28, 29 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
         });
     
+        this.anims.create({
+            key: 'walkLeftBottom',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 30, 31, 32, 33, 34, 35 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkRightBottom',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 36, 37, 38, 39, 40, 41 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkBottom',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 42, 43, 44, 45, 46, 47 ] }),
+            frameRate: this.animationFrameRate,
+            repeat: -1
+        });
+
         const cody = this.add.sprite(600, 370);
         cody.setScale(1);
-        cody.play('walk'); // https://phaser.io/examples/v3/view/animation/create-animation-from-sprite-sheet
+        cody.play('walkRightBottom');
     }
 }
 
